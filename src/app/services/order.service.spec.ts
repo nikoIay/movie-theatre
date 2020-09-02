f6fc86fd76218af90dd84ea5a4f870266717f7aa
nikoIay <ignatiev.ns@gmail.com>
2020-09-02 20:22:42 +0300

    Add some tests

--- src/app/services/order.service.spec.ts
+++ src/app/services/order.service.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { OrderService } from './order.service';
+import {OrderService} from './order.service';
+import {initialState} from '../store/reducers/product';
+import {Order, OrderItem} from '../models/order';
 
 describe('OrderService', () => {
-  let service: OrderService;
+    let service: OrderService;
+    const order: Order = initialState.products.map((p): OrderItem => ({quantity: 5, product: p}));
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrderService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(OrderService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('calling countOrderTotalSum', () => {
+        const totalSums = service.countOrderItemTotalSums(order, initialState.discounts);
+        const result = service.countOrderTotalSum(totalSums);
+        expect(result).toEqual(37);
+    });
 });
--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ProductService } from './product.service';
+import {ProductService} from './product.service';
+import {Product} from '../models/product';
 
 describe('ProductService', () => {
-  let service: ProductService;
+    let service: ProductService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ProductService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('calling countDiscountedOrderItemPrice', () => {
+        const product: Product = {
+            name: 'Product',
+            price: 2,
+            id: 1
+        };
+
+        const result = service.countDiscountedOrderItemPrice(
+            {product, quantity: 5},
+            {from: 2, to: 1}
+            );
+
+        expect(result).toEqual(6);
+    });
 });
